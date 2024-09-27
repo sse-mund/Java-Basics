8e0284e9366896ba3135b34eea90ca5c0ebea5d7
Madhu Mundhra <mundhra.madhu@gmail.com>
2024-09-27 12:33:05 -0500

    enhancement to java programs

--- src/main/java/org/example/SwictchCase.java
+++ src/main/java/org/example/SwictchCase.java
@@ -1,4 +1,20 @@
 package main.java.org.example;
 
+import static java.lang.Math.*;
+
 public class SwictchCase {
+    public static void main(String[] args){
+
+    }
+
+    private static Number performOperation(int a, int b, char opr){
+        return switch (opr){
+            case('+')-> a+b;
+            case('*')-> a*b;
+            case('-') -> Math.abs(a-b);
+            case('/') -> a/b;
+            default -> throw new IllegalStateException("Unexpected value: " + opr);
+        };
+    }
+
 }
